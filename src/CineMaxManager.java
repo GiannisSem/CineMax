976a6b666bc8860cfdca1e91047793612c3e28ef
Sem <119437317+GiannisSem@users.noreply.github.com>
2026-05-20 15:43:56 +0200

    Visualizza Proiezione,Prenotazione

--- src/CineMaxManager.java
+++ src/CineMaxManager.java
@@ -4,9 +4,10 @@ import java.util.List;
 public class CineMaxManager {
     // tutta ricerca e funzionalità
 
-    private static List<Proiezione> listaProiezioni = FileManager.deserializza_proiezioni();
-    private static List<Film> listaFilm = FileManager.deserializza_film();
-    private static List<Utente> listaUtenti = FileManager.deserializza_utenti();
+    private static final List<Proiezione> listaProiezioni = FileManager.deserializza_proiezioni();
+    private static final List<Film> listaFilm = FileManager.deserializza_film();
+    private static final List<Utente> listaUtenti = FileManager.deserializza_utenti_csv();
+    private static final List<Prenotazione> listaPrenotazioni = FileManager.deserializza_prenotazioni_csv();
 
     public static List<Proiezione> getListaProiezioni(){
         return listaProiezioni;
@@ -17,6 +18,8 @@ public class CineMaxManager {
     public static List<Utente> getListaUtenti(){
         return listaUtenti;
     }
+
+    /* CERCA PROIEZIONE */
 
     public static List<Proiezione> cercaProiezione_Titolo(List<Proiezione> lista, String titolo) {
         List<Proiezione> filtrata = new ArrayList<>();
@@ -62,9 +65,50 @@ public class CineMaxManager {
         return filtrata;
     }
 
-
-
     public static List<Proiezione> resetFiltri(){
         return getListaProiezioni();
     }
+
+    /* VISUALIZZA PROIEZIONE */
+    public static String visualizzaProiezione(List<Proiezione> lista){
+        if (lista == null)
+            return "";
+
+        StringBuilder sb = new StringBuilder(lista.get(0).toInfo());
+        for (int i = 1; i < lista.size(); i++) {
+            sb.append("\n").append(lista.get(i).toInfo());
+        }
+        return sb.toString();
+    }
+
+    /* REGISTRA CLIENTE */
+    public static boolean registraCliente(String[] params){
+        // todo: da fare per ogni tipo (più metodi)
+        return false;
+    }
+
+    /* Prenotazioni */
+    public static Prenotazione cercaPrenotazione(List<Prenotazione> lista, int id) {
+        int inizio = 0;
+        int fine = lista.size()-1;
+
+        while (inizio <= fine){
+            int centro = inizio + (fine - inizio) / 2;
+            int idCentrale = lista.get(centro).getCodicePrenotazione();
+
+            if (idCentrale == id)
+                return lista.get(centro);
+
+            if (idCentrale > id)
+                fine = centro+1;
+
+            if (idCentrale < id)
+                inizio = centro+1;
+        }
+
+        return null;
+    }
+
+    // public static String visualizzaPrenotazione()
+
 }
